043012bd70a9a715a2694652bb01c6b75f10f8c6
Rodrigo L. <cabelin2104@gmail.com>
2025-02-10 15:40:59 -0300

    O mesmo código, mas com o try-with-resources

--- arquivosFR_BR/src/application/program.java
+++ arquivosFR_BR/src/application/program.java
@@ -7,10 +7,7 @@ import java.io.IOException;
 public class program {
 	public static void main(String[]args) {
 		String path = "c:\\ProjetosJava\\arquivosScanner\\in.txt";
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(path));
-			
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 			while(line != null) {
 				System.out.println(line);
@@ -19,18 +16,6 @@ public class program {
 		}
 		catch(IOException e){
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			//O try-catch é usado porque quanto o programa tentar fechar o br e o fr pode dar a exceção IOException,
-			//e como ela é uma extensão da classe exception, devemos trata-la.
-			try {
-				if(br != null) {
-					br.close();
-				}
-			}
-			catch(IOException e){
-				e.printStackTrace();
-			}
 		}
 	}
 }
