3c148de8119c3012db8878760575d86bb3905910
Rodrigo L. <cabelin2104@gmail.com>
2025-02-11 10:15:01 -0300

    Adição de mais algumas operações

--- arquivosTesteFile/src/application/program.java
+++ arquivosTesteFile/src/application/program.java
@@ -11,18 +11,26 @@ public class program {
 		
 		File file = new File(path);
 		
+		System.out.println("Seu caminho: " + file.getPath());
+		System.out.println("Seu pai: " + file.getParent());
+		System.out.println("Seu nome: " + file.getName());
+		
+		System.out.println(" ");
+		
 		File[] folders = file.listFiles(File::isDirectory);
 		System.out.println("Folders:");
 		for(File folder : folders) {
 			System.out.println(folder);
 		}
 		
+		System.out.println(" ");
 		File[] files = file.listFiles(File::isFile);
 		System.out.println("Files:");
 		for(File fil : files) {
 			System.out.println(fil);
 		}
 		
+		System.out.println(" ");
 		boolean sucesso = new File(path + "\\NovaPasta").mkdir();
 		System.out.println("Criou pasta: " + sucesso);
 		sc.close();
