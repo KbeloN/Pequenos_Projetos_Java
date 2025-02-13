e5aed4a8fc9156b0d46a17f6026d0c8fec61f748
Rodrigo L. <cabelin2104@gmail.com>
2025-02-13 09:06:55 -0300

    mudanças na dependencia na classe rentalservice

--- Interface/src/model/services/RentalService.java
+++ Interface/src/model/services/RentalService.java
@@ -10,30 +10,16 @@ public class RentalService {
 	private Double pricePerHour;
 	private Double pricePerDay;
 	
-	private BrazilTaxService taxService;
+	private TaxService taxService;
 	
 	
-	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
 		this.taxService = taxService;
 	}
 	
-	public Double getPricePerHour() {
-		return pricePerHour;
-	}
-	public void setPricePerHour(Double pricePerHour) {
-		this.pricePerHour = pricePerHour;
-	}
-	public Double getPricePerDay() {
-		return pricePerDay;
-	}
-	public void setPricePerDay(Double pricePerDay) {
-		this.pricePerDay = pricePerDay;
-	}
-	
 	public void processInvoice(CarRental carRental) {
-		
 		
 		Double minutes = (double) Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
 		Double hours = minutes / 60.0;
